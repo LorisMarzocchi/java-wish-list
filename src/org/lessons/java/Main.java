a7d7f9ba5eb9a30766b1851f572d159a59470ca1
LorisMarzocchi <ml77182a@gmail.com>
2023-10-26 15:59:55 +0200

    first commit

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,9 @@
 package org.lessons.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,10 +15,32 @@ public class Main {
 
         List<Regalo> listaRegali = new ArrayList<>();
 
+        //      SCANNER (READ FILE)
+
+        Scanner reader = null;
+        int numeroDiRegali = 0;
+
+
+        try {
+            reader = new Scanner(new File("./resources/regali.txt"));
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+//                System.out.println(line);
+                numeroDiRegali++;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        };
+
         boolean exit = false;
 
         while (!exit) {
-            System.out.println("insert new person y/n");
+            System.out.println("Ci sono " + + numeroDiRegali + " regali nella lista. Vuoi inserire un nuovo regalo y/n");
             exit = scanner.nextLine().equals("n");
             if (!exit) {
                 System.out.println("descrizione regalo: ");
@@ -22,13 +48,40 @@ public class Main {
                 System.out.println("destinatario regalo");
                 String destinatario = scanner.nextLine();
                 listaRegali.add(new Regalo(descrizione, destinatario));
-
-
+                numeroDiRegali++;
             }
-            System.out.println("Lista Regali: " + listaRegali);
-
 
         }
+
+//            System.out.println("Lista Regali (ArrayList appena creata):\n" + listaRegali);
+
+            for (Regalo regalo : listaRegali) {
+                System.out.println("\n" + regalo);
+            }
+
+
+//      WRITE FILE
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter("./resources/regali.txt",true); //ultimo parametro append true aggiunge nuovi regali alla fine del file, lasciando intatti i regali esistenti
+                for (Regalo regalo : listaRegali) {
+                    writer.write(regalo.toString() + " ");
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); //Questo mostra lo stack trace completo dell'errore
+                System.out.println("Unable to write to the file");
+            } finally {
+                try {
+                    if (writer != null) {
+                        System.out.println("File write.... Closing the file\n");
+                        writer.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
             scanner.close();
     }
 }
